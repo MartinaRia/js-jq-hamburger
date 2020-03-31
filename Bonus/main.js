4627111c31791940d17729d207bcc4f3ab202b32
Martina <martina.ria@hotmail.com>
2020-03-31 19:01:17 +0200

    Bonus complete

---
+++ Bonus/main.js
@@ -0,0 +1,17 @@
+/*BONUS:
+cartella a parte;
+cerco di ricreare la cosa facendo un mio html;
+e conseguente js, in base alla mia struttura.*/
+
+//open div hamburger-menu on click of fa-bars
+$('.fa-bars').click(
+  function(){
+    $('.mobile-vers').toggle(800);
+  }
+)
+//close div hamburger-menu on click of fa-times
+// $('.fa-times').click(
+//   function(){
+//     $('.hamburger-menu').toggle('fast', 'swing');
+//   }
+// )
